63bd0a328383a7fe98a73002e779f8884d1345d1
Rehmankhalil <hawka1994@gmail.com>
2015-04-28 13:20:14 +0500

    prototype

---
+++ Minesweeper/Minesweeper/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void smileyButton_Click(object sender, EventArgs e)
+        {
+            //Image objectImg = Image.FromFile("C:\\Users\\NCLab-04\\Desktop\\2000px-Day-template.svg.png");
+            //smileyButton.Image= objectImg;
+            //smileyButton.Width = objectImg.Width;
+            //smileyButton.Height = objectImg.Height;
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
